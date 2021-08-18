057d1cea1faff905971981f3d983a25250490390
Lee Si Young <66060749+Shung-2@users.noreply.github.com>
2021-08-18 11:41:08 +0900

    210818 study
    탐색, 순차탐색, 자기 구성법(전진 이동법, 전위법, 빈도 계수법)

---
+++ Algorithms/LinkedList.h
@@ -0,0 +1,35 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> /* New! */
+
+typedef int ElementType;
+
+typedef struct tagNode
+{
+    ElementType Data;
+    struct tagNode* NextNode;
+} Node;
+
+/* 함수 원형 선언 */
+Node* SLL_CreateNode(ElementType NewData);
+void  SLL_DestroyNode(Node* Node);
+void  SLL_AppendNode(Node** Head, Node* NewNode);
+void  SLL_InsertAfter(Node* Current, Node* NewNode);
+void  SLL_InsertNewHead(Node** Head, Node* NewHead);
+void  SLL_RemoveNode(Node** Head, Node* Remove);
+Node* SLL_GetNodeAt(Node* Head, int Location);
+int   SLL_GetNodeCount(Node* Head);
+
+/* P226 전진이동법 함수 원형 & Vitamin Quiz */
+//Node* SLL_MoveToFront(Node** Head, int Target); /* 원형 */
+int SLL_MoveToFront(int DataSet[], int Length, int target);
+
+/* P228 전위법 함수 원형 & Vitamin Quiz */
+//Node* SLL_Transpose(Node** Head, int Target);
+void Swap(int* A, int* B);
+int SLL_Tranpose(int DataSet[], int Length, int target);
+
+#endif
