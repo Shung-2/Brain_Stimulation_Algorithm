f722f317254d20bfa9601771f75bb862f9102090
Lee Si Young <66060749+Shung-2@users.noreply.github.com>
2021-08-27 10:57:43 +0900

    210827 study
    그래프 정의, 정점, 간선, 인접, 경로, 사이클, 연결성, 표현 방법(인접 행렬, 인접 리스트), 인접 리스트 구현

--- Algorithms/test.cpp
+++ Algorithms/test.cpp
@@ -6,26 +6,20 @@
 //    cin.tie(NULL);
 //    ios::sync_with_stdio(false);
 //
-//    int n = 0, ArrayNum = 0;
-//    
-//    cin >> n;
-//    int Array[n];
-//
-//    for (int i = 0; i < n; i++)
-//    {
-//        cin >> ArrayNum;
-//        Array[i] = ArrayNum;
-//    }
-//
-//    int Min = Array[0];
+//    int Array[9];
 //    int Max = Array[0];
-//
-//    for (int i = 0; i < n; i++)
+//    int Count = 0;
+//    
+//    for (int i = 0; i < 9; i++)
 //    {
-//        if (Array[i] < Min) Min = Array[i];
-//        if (Array[i] > Max) Max = Array[i];
+//        cin >> Array[i];
+//        if (Array[i] > Max)
+//        {
+//            Max = Array[i];
+//            Count = i + 1;
+//        }
 //    }
+//    cout << Max << "\n" << Count;
 //
-//    cout << Min << " " << Max;
 //    return 0;
 //}
