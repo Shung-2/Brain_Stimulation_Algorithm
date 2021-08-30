7c5458be3779c329162727fd2a2f8a3c79a509ca
Shung-2 <66060749+Shung-2@users.noreply.github.com>
2021-08-30 22:55:35 +0900

    210829 study
    그래프 순회, 깊이 우선 탐색, 너비 우선 탐색, 그래프 순회 예제 프로그램

--- Algorithms/LinkedQueue.c
+++ Algorithms/LinkedQueue.c
@@ -1,12 +1,23 @@
 #include "LinkedQueue.h"
 
-void LQ_CreateQueue(LinkedQueue** Queue)
+//void LQ_CreateQueue(LinkedQueue** Queue)
+//{
+//	/* 큐를 자유저장소에 생성 */
+//	(*Queue) = (LinkedQueue*)malloc(sizeof(LinkedQueue));
+//	(*Queue)->Front = NULL;
+//	(*Queue)->Rear = NULL;
+//	(*Queue)->Count = 0;
+//}
+
+LinkedQueue* LQ_CreateQueue()
 {
+	LinkedQueue* Queue = (LinkedQueue*)malloc(sizeof(LinkedQueue));
 	/* 큐를 자유저장소에 생성 */
-	(*Queue) = (LinkedQueue*)malloc(sizeof(LinkedQueue));
-	(*Queue)->Front = NULL;
-	(*Queue)->Rear = NULL;
-	(*Queue)->Count = 0;
+	Queue->Front = NULL;
+	Queue->Rear = NULL;
+	Queue->Count = 0;
+
+	return Queue;
 }
 
 void LQ_DestroyQueue(LinkedQueue* Queue)
@@ -21,15 +32,25 @@ void LQ_DestroyQueue(LinkedQueue* Queue)
 	free(Queue);
 }
 
-Node* LQ_CreateNode(char* NewData)
+//Node* LQ_CreateNode(char* NewData)
+//{
+//	Node* NewNode = (Node*)malloc(sizeof(Node));
+//	NewNode->Data = (char*)malloc(strlen(NewData) + 1);
+//
+//	strcpy(NewNode->Data, NewData); /* 데이터를 저장한다. */
+//
+//	NewNode->NextNode = NULL; /* 다음 노드에 대한 포인터는 NULL로 초기화한다. */
+//
+//	return NewNode; /* 노드의 주소를 반환한다. */
+//}
+
+Node* LQ_CreateNode(Vertex* V)
 {
 	Node* NewNode = (Node*)malloc(sizeof(Node));
-	NewNode->Data = (char*)malloc(strlen(NewData) + 1);
-
-	strcpy(NewNode->Data, NewData); /* 데이터를 저장한다. */
+	NewNode->Data = V;
 
 	NewNode->NextNode = NULL; /* 다음 노드에 대한 포인터는 NULL로 초기화한다. */
-
+	
 	return NewNode; /* 노드의 주소를 반환한다. */
 }
 
@@ -39,38 +60,74 @@ void LQ_DestroyNode(Node* _Node)
 	free(_Node);
 }
 
-void LQ_Enqueue(LinkedQueue* Queue, Node* NewNode)
+//void LQ_Enqueue(LinkedQueue* Queue, Node* NewNode)
+//{
+//	if (Queue->Front == NULL)
+//	{
+//		Queue->Front = NewNode;
+//		Queue->Rear = NewNode;
+//		Queue->Count++;
+//	}
+//	else
+//	{
+//		Queue->Rear->NextNode = NewNode;
+//		Queue->Rear = NewNode;
+//		Queue->Count++;
+//	}
+//}
+
+//Node* LQ_Dequeue(LinkedQueue* Queue)
+//{
+//	/* LQ_Dequeue() 함수가 반환할 최상위 노드 */
+//	Node* Front = Queue->Front;
+//
+//	if (Queue->Front->NextNode == NULL)
+//	{
+//		Queue->Front = NULL;
+//		Queue->Rear = NULL;
+//	}
+//	else
+//	{
+//		Queue->Front = Queue->Front->NextNode;
+//	}
+//
+//	Queue->Count--;
+//
+//	return Front;
+//}
+
+void LQ_Enqueue(LinkedQueue** Queue, Node* NewNode)
 {
-	if (Queue->Front == NULL)
+	if ((*Queue)->Front == NULL)
 	{
-		Queue->Front = NewNode;
-		Queue->Rear = NewNode;
-		Queue->Count++;
+		(*Queue)->Front = NewNode;
+		(*Queue)->Rear = NewNode;
+		(*Queue)->Count++;
 	}
 	else
 	{
-		Queue->Rear->NextNode = NewNode;
-		Queue->Rear = NewNode;
-		Queue->Count++;
+		(*Queue)->Rear->NextNode = NewNode;
+		(*Queue)->Rear = NewNode;
+		(*Queue)->Count++;
 	}
 }
 
-Node* LQ_Dequeue(LinkedQueue* Queue)
+Node* LQ_Dequeue(LinkedQueue** Queue)
 {
 	/* LQ_Dequeue() 함수가 반환할 최상위 노드 */
-	Node* Front = Queue->Front;
+	Node* Front = (*Queue)->Front;
 
-	if (Queue->Front->NextNode == NULL)
+	if ((*Queue)->Front->NextNode == NULL)
 	{
-		Queue->Front = NULL;
-		Queue->Rear = NULL;
+		(*Queue)->Front = NULL;
+		(*Queue)->Rear = NULL;
 	}
 	else
 	{
-		Queue->Front = Queue->Front->NextNode;
+		(*Queue)->Front = (*Queue)->Front->NextNode;
 	}
 
-	Queue->Count--;
+	(*Queue)->Count--;
 
 	return Front;
 }
--- Algorithms/LinkedQueue.h
+++ Algorithms/LinkedQueue.h
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "Graph.h" //new
 
 typedef struct tagNode
 {
-	char* Data;
+	//char* Data;
+	Vertex*			Data;
 	struct tagNode* NextNode;
 } Node;
 
@@ -18,14 +20,19 @@ typedef struct tagLinkedQueue
 	int Count;
 } LinkedQueue;
 
-void	LQ_CreateQueue(LinkedQueue** Queue);
-void	LQ_DestroyQueue(LinkedQueue* Queue);
+//void	LQ_CreateQueue(LinkedQueue** Queue);
+LinkedQueue* LQ_CreateQueue();
+void		 LQ_DestroyQueue(LinkedQueue* Queue);
 
-Node*	LQ_CreateNode(char* NewData);
+//Node*	LQ_CreateNode(char* NewData);
+Node*	LQ_CreateNode(Vertex* V);
 void	LQ_DestroyNode(Node* _Node);
 
-void	LQ_Enqueue(LinkedQueue* Queue, Node* NewNode);
-Node*	LQ_Dequeue(LinkedQueue* Queue);
+//void	LQ_Enqueue(LinkedQueue* Queue, Node* NewNode);
+//Node*	LQ_Dequeue(LinkedQueue* Queue);
+void	LQ_Enqueue(LinkedQueue** Queue, Node* NewNode);
+Node*	LQ_Dequeue(LinkedQueue** Queue);
+
 
 int		LQ_IsEmpty(LinkedQueue* Queue);
 
