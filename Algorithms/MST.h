83b86b02bbecc6b1681fca4e8be0b7f471cd6ca3
Shung-2 <66060749+Shung-2@users.noreply.github.com>
2021-09-01 00:27:13 +0900

    210831 study
    최소 신장 트리, 프림 알고리즘 개념, 크루스칼 알고리즘 개념, 최소 신장 트리 예제 구현(프림, 크루스칼)

---
+++ Algorithms/MST.h
@@ -0,0 +1,13 @@
+#ifndef HST_H
+#define HST_H
+
+#include "Graph.h"
+#include "PriorityQueue.h"
+#include "DisjointSet.h"
+
+#define MAX_WEIGHT 36267
+
+void Prim(Graph* G, Vertex* StartVertex, Graph* MST);
+void Kruskal(Graph* G, Graph* MST);
+
+#endif // !HST_H
