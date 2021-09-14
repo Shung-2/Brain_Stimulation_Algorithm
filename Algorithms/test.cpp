06ae61cafb028aa28c2839a748340e7942bb9cd3
Lee Si Young <66060749+Shung-2@users.noreply.github.com>
2021-09-14 16:38:21 +0900

    210914 study
    알고리즘 성능 분석, 알고리즘 우수함을 가리는 기준, 수행 시간의 분석, 점근 표기법, 재귀 알고리즘 성능 분석 등

--- Algorithms/test.cpp
+++ Algorithms/test.cpp
@@ -1,23 +1,69 @@
-//#include <iostream>
-//#include <vector>
-//using namespace std;
-//
-//long long sum(vector<int>& a)
+#include <iostream>
+using namespace std;
+
+//int Find_Hansu(int Num)
 //{
-//    long long Result = 0;
+//    int Count = 0;
 //
-//    for (int i = 0; i < a.size(); i++)
-//        Result += a[i];
+//    for (int i = 0; i < Num; i++)
+//    {
+//        int First_Num = Num / 100;
+//        int Second_Num = Num % 100 / 10;
+//        int Third_Num = Num % 100 % 10;
 //
-//    return Result;
+//        if (Num < 99)
+//        {
+//            Count++;
+//        }
+//        else
+//        {
+//            if ((Third_Num - Second_Num) == (Second_Num - First_Num))
+//            {
+//                Count++;
+//            }
+//        }
+//    }
+//
+//    return Count;
 //}
-//
-//int main()
-//{
-//    cin.tie(NULL);
-//    ios::sync_with_stdio(false);
-//
-//    //
-//
-//    return 0;
-//}
+
+bool Hansu(int Num)
+{
+    if (Num < 100)
+        return true;
+
+    int First_Num = Num / 100;
+    int Second_Num = Num % 100 / 10;
+    int Third_Num = Num % 100 % 10;
+
+    if ((Third_Num - Second_Num) == (Second_Num - First_Num))
+        return true;
+
+    return false;
+}
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    //int N = 0, Result = 0;
+    //cin >> N;
+    //Result = Find_Hansu(N);
+    //cout << Result;
+
+    int N = 0, Count = 0;
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (Hansu(i))
+        {
+            Count++;
+        }
+    }
+    
+    cout << Count;
+
+    return 0;
+}
